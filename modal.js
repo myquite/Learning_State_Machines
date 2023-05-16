4bc2a23038fe897fef18b32eff88eff23ca8406e
Mike White <myquite@gmail.com>
2023-05-16 20:45:20 +0000

    added TODO

--- modal.js
+++ modal.js
@@ -1,40 +1,40 @@
 const modal = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".close");
 const modalOpen = document.querySelector("#delete");
 const modalCancel = document.querySelector("#cancel");
 const modalConfirm = document.querySelector("#confirm");
 
-// Defines the modal states
 const ModalStates = {
     CLOSED: "closed",
     OPENED: "opened",
     TRANSITION: "transition"
-    };
+};
 
-// Defines the modal object
+    //TODO: Find bug that causes modal to open twice
+
 const modalObject = {
     state: ModalStates.CLOSED,
     changeState: function () {
         switch (this.state) {
             case ModalStates.CLOSED:
                 this.state = ModalStates.OPENED;
-                modal.classList.add("active");
-                modalOverlay.classList.add("active");
                 modalClose.addEventListener("click", () => this.changeState());
-                modalCancel.addEventListener("click", () => this.changeState());
+                modal.classList.add("activeModal");
                 break;
             case ModalStates.OPENED:
                 this.state = ModalStates.TRANSITION;
-                modal.classList.remove("active");
-                modalOverlay.classList.remove("active");
+                modal.classList.remove("activeModal");
+                setTimeout(() => this.changeState(), 500);
                 break;
             case ModalStates.TRANSITION:
                 this.state = ModalStates.CLOSED;
+                modalClose.removeEventListener("click", () => this.changeState());
                 break;
+            default:
+                console.log("Error: Modal state not found \n")
         }
     }
 };
 
-// Start the modal state machine
+
 modalOpen.addEventListener("click", () => modalObject.changeState());
